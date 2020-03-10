032a83239580371d95e17a4015f2e5756c76fcc5
azuspan607 <aa.zuspan@gmail.com>
2020-03-10 12:55:12 -0700

    Add threading to run background visualizations with mock run_vis function (to be implemented later)

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,26 @@
 from flask import request, jsonify
+import time
+import threading
+import logging
 
 from backend.Controller import Controller
 from backend.app import app
 
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Signal to stop running visualization threads
+stop_vis_thread = False
+
+# Gives running visualization thread time to die before starting new thread. 
+# If set too low, multiple threads will run simultaneously. Time must be 
+# less than max time between stop_vis_thread checks in vis loops.
+thread_kill_time = 1
+
 @app.route('/', methods=['GET'])
 def index():
+    global stop_vis_thread
+
     response = {}
 
     if request.method == 'GET':
@@ -17,7 +33,20 @@ def index():
             response['list'] = vis_list
 
         elif request.args['type'] == 'startVis':
-            # TODO: Send name to Controller to render this vis
-            print(request.args['visName'])
+            stop_vis_thread = True
+            # Give previous thread enough time to die. Sleep time must be less than  on the max length of the vis loop
+            time.sleep(thread_kill_time)
+
+            vis_thread = threading.Thread(target=run_vis, args=(request.args['visName'],))
+            vis_thread.start()
 
     return jsonify(response)
+
+# TODO: Replace mock function with call to Controller to run vis.render based on vis name
+def run_vis(name):
+    global stop_vis_thread
+    stop_vis_thread = False
+
+    while not stop_vis_thread:
+        logging.debug(f'running {name}')
+        time.sleep(0.5)
