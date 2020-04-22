0053fb0e0892edadc5e24ad70655c0b612b874a5
aazuspan <aa.zuspan@gmail.com>
2020-04-22 11:15:11 -0700

    Simplify color shifting by interpolating between fill color and max color, removing min color

--- backend/visualizations/Other/Fireflies.py
+++ backend/visualizations/Other/Fireflies.py
@@ -9,9 +9,8 @@ class Fireflies(Visualization):
     name = 'Fireflies'
     description = 'Slowly fading green-yellow pixels over a dark background.'
 
-    min_color = (0, 0, 0)
     max_color = (150, 255, 0)
-    fill_color = (0, 0, 0)
+    fill_color = (5, 0, 10)
     fade_in_length = 750
     fade_out_length = 1500
     
@@ -24,19 +23,20 @@ class Fireflies(Visualization):
         self.pixels.show()
 
         firefly_index = random.randint(0, constants.PIXEL_COUNT - 1)
+        firefly_index = 0
 
         # Set starting color
-        faded_color = self.min_color
+        faded_color = self.fill_color
         
         # Fade firefly color in
-        for i in range(0, self.fade_in_length):
-            faded_color = utils.interpolate_color(self.min_color, self.max_color, i/self.fade_in_length)
+        for i in range(self.fade_in_length):
+            faded_color = utils.interpolate_color(self.fill_color, self.max_color, i/self.fade_in_length)
             self.pixels[firefly_index] = utils.floatcolor2intcolor(faded_color)
             self.pixels.show()
         
         # Fade firefly color out
-        for i in range(0, self.fade_out_length):
-            faded_color = utils.interpolate_color(self.max_color, self.min_color, i/self.fade_out_length)
+        for i in range(self.fade_out_length):
+            faded_color = utils.interpolate_color(self.max_color, self.fill_color, i/self.fade_out_length)
             self.pixels[firefly_index] = utils.floatcolor2intcolor(faded_color)
             self.pixels.show()
             
