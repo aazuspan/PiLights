7da2c5039152692b7356dcc80466010cc8b5b8e8
aazuspan <aa.zuspan@gmail.com>
2020-05-28 11:26:33 -0700

    Add lightning

--- backend/visualizations/Sky/Rain.py
+++ backend/visualizations/Sky/Rain.py
@@ -12,11 +12,13 @@ class Rain(Visualization):
     
     fill_color = (0, 5, 15)
     
-    min_raindrop_chance = 0.0005
+    min_raindrop_chance = 0.00001
     max_raindrop_chance = 0.05
     
     min_raindrop_duration = 15
     max_raindrop_duration = 100
+    
+    lightning_chance = 0.01
     
     def __init__(self, pixels):
         super().__init__(pixels)
@@ -26,9 +28,14 @@ class Rain(Visualization):
         self.perlin_noise = opensimplex.OpenSimplex()
         
         self.intensity = 0.0
+        self.lightning = None
 
         self.array = [random.choice([None, Raindrop(self, i)]) for i in range(constants.PIXEL_COUNT)]
     
+    def generate_lightning(self):
+        if random.random() < self.lightning_chance and not self.lightning:
+            self.lightning = Lightning(self)
+        
     def update(self):
         """
         Update rain intensity using Perlin noise
@@ -36,6 +43,7 @@ class Rain(Visualization):
         noise = self.perlin_noise.noise2d(self.noise_index, 0)
         self.intensity = utils.remap(noise, -1, 1, 0, 1)
         self.noise_index += self.noise_interval
+        self.intensity = 1
     
     @property
     def raindrop_chance(self):
@@ -62,10 +70,16 @@ class Rain(Visualization):
                 self.array[i] = Raindrop(self, i)
     
     def render(self):
-        self.update()
         self.pixels.fill(self.fill_color)
-        self.generate_raindrops()
         
+        self.update()
+        self.generate_lightning()
+        
+        if self.lightning:
+            self.lightning.render()
+
+        self.generate_raindrops()
+            
         for cell in self.array:
             if cell:
                 cell.fade()
@@ -74,11 +88,46 @@ class Rain(Visualization):
         self.pixels.show()
 
 
+class Lightning:
+    duration = 50
+    max_color = (255, 255, 255)
+    min_color = (0, 0, 0)
+    
+    def __init__(self, parent):
+        self.parent = parent
+        self.rounds = 0
+    
+    @property
+    def color(self):
+        """
+        Interpolate lightning color between max and min based on number of rounds
+        """
+        return utils.interpolate_color(self.max_color, self.min_color, self.rounds / self.duration)
+    
+    def render(self):
+        """
+        Render all background cells as lightning
+        """
+        for i, cell in enumerate(self.parent.array):
+            self.parent.pixels[i] = self.color
+        
+        self.rounds += 1
+        
+        if self.rounds >= self.duration:
+            self.remove()
+    
+    def remove(self):
+        """
+        Remove the lightning from the parent rainstorm
+        """
+        self.parent.lightning = None
+
+
 class Raindrop:
     color_range = {
         0: (200, 255, 255),
-        1: (100, 150, 255),
-        2: (50, 100, 150),
+        1: (75, 125, 255),
+        2: (40, 75, 150),
         3: (0, 50, 75),
         4: (0, 20, 50),
         5: (0, 5, 15),
