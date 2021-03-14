c0b490398761b960eb53b358ee4438ae62b7a633
aazuspan <aa.zuspan@gmail.com>
2021-03-14 12:46:41 -0700

    Kill servers on exit

--- scripts/run_lights.sh
+++ scripts/run_lights.sh
@@ -9,15 +9,18 @@ ignore_wemos=0
 while getopts "dw" OPTION
 do
 	case "${OPTION}" in
-		d) dev_mode=1;;
-		w) ignore_wemos=1;;
+		d) dev_mode=1; 
+		echo "Starting in development mode. Functionality will be limited for compatibility.";;
+		w) ignore_wemos=1;
+		echo "Skipping Wemo discovery.";;
 	esac
 done
 export dev_mode="$dev_mode";
 export ignore_wemos=$ignore_wemos;
 
-
+# Spin up frontend and backend
 cd ~/PiLights/frontend; serve -l 3000 -s build &
 cd ~/PiLights/backend; sudo -E flask run --host=192.168.7.99
 
-exit 0
+# Kill the frontend and backend if the terminal is exited
+trap 'kill 0 & echo "Killing all processes..."' EXIT
